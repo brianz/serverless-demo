cccda988e257423348c87c87983a4f7c4add401d
Brian Zambrano <brianz@gmail.com>
2016-06-02 15:23:41 -0600

    Output stage name

--- serverless-demo/src/hello/handler.py
+++ serverless-demo/src/hello/handler.py
@@ -3,17 +3,21 @@ from __future__ import print_function
 import json
 import logging
 
+import os
+
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 def handler(event, context):
+    value = os.environ.get('SERVERLESS_STAGE', '')
     return """<html>
     <head></head>
     <body>
         <h1>Hello from Serverless/API Gateway/Lambda</h1>
+        <h2>SERVERLESS_STAGE: %s</h2>
         <h2>Event:</h2>
         <pre>
             %s
         </pre>
         </body>
-    </html>""" % (json.dumps(event), )
+    </html>""" % (value, json.dumps(event), )
