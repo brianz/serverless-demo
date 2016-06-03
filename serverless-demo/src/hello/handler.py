91956b024807e6dfc8bf1921555b255688ef3227
Brian Zambrano <brianz@gmail.com>
2016-06-02 22:17:37 -0600

    Add magic variable

--- serverless-demo/src/hello/handler.py
+++ serverless-demo/src/hello/handler.py
@@ -10,14 +10,16 @@ log.setLevel(logging.DEBUG)
 
 def handler(event, context):
     value = os.environ.get('SERVERLESS_STAGE', '')
+    magic = os.environ.get('MAGIC_VARIABLE', '')
     return """<html>
     <head></head>
     <body>
         <h1>Hello from Serverless/API Gateway/Lambda</h1>
         <h2>SERVERLESS_STAGE: %s</h2>
+        <h2>MAGIC_VARIABLE: %s</h2>
         <h2>Event:</h2>
         <pre>
             %s
         </pre>
         </body>
-    </html>""" % (value, json.dumps(event), )
+    </html>""" % (value, magic, json.dumps(event), )
